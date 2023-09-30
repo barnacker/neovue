a3e19ad8fec6269f6f52fbca012aa8ccaa2eb49e
barnacker <habib.tremblay@gmail.com>
2023-09-29 22:46:48 -0400

    Settings menu item

--- lua/plugins/bootScreen.lua
+++ lua/plugins/bootScreen.lua
@@ -1,12 +1,12 @@
 return {
-	  {
-    "goolord/alpha-nvim",
-    lazy = true,
-    event = "VimEnter",
-    config = function()
-      require "alpha".setup(require "alpha.themes.dashboard".config)
-      local dashboard = require("alpha.themes.dashboard")
-      local logo = [[
+	{
+		"goolord/alpha-nvim",
+		lazy = true,
+		event = "VimEnter",
+		config = function()
+			require "alpha".setup(require "alpha.themes.dashboard".config)
+			local dashboard = require("alpha.themes.dashboard")
+			local logo = [[
 
 ███╗  ██╗███████╗ █████╗ ██╗   ██╗██╗   ██╗███████╗
 ████╗ ██║██╔════╝██╔══██╗██║   ██║██║   ██║██╔════╝
@@ -16,8 +16,18 @@ return {
 ╚═╝  ╚══╝╚══════╝ ╚════╝    ╚═╝    ╚═════╝ ╚══════╝
 ]]
 
-      dashboard.section.header.val = vim.split(logo, "\n")
-      --dashboard.section.buttons.val = {}
-    end,
-  },
+			dashboard.section.header.val = vim.split(logo, "\n")
+			dashboard.section.buttons.val = {
+				dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
+				dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
+				dashboard.button("r", "  Recent", ":Telescope oldfiles<CR>"),
+				dashboard.button("s", "  Settings", ":cd ~/.config/nvim/<CR> | :e ~/.config/nvim/README.md<CR>"),
+			}
+			dashboard.section.footer.val = " ue IDE for 'Reals' 󰟀 "
+
+			dashboard.config.opts.noautocmd = true
+
+			vim.cmd [[autocmd User AlphaReady echo 'ready']]
+		end,
+	},
 }
