5010e038e27e18bb9626ada2f2e55efd158ca1a6
barnacker <habib.tremblay@gmail.com>
2023-10-09 02:10:57 -0400

    Minor fixes

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -169,6 +169,7 @@ return {
 		version = "*", -- Use for stability; omit to use `main` branch for the latest features
 		event = "VeryLazy",
 		config = function()
+			---@diagnostic disable-next-line: missing-fields
 			require("nvim-surround").setup({
 				-- Configuration here, or leave empty to use defaults
 			})
