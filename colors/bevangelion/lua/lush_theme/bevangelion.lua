18d42e462a922dec11eff856d095f49373a57da1
barnacker <habib.tremblay@gmail.com>
2023-10-16 15:45:53 -0400

    Search color

--- colors/bevangelion/lua/lush_theme/bevangelion.lua
+++ colors/bevangelion/lua/lush_theme/bevangelion.lua
@@ -140,9 +140,9 @@ local theme = lush(function(injected_functions)
 		-- PmenuExtraSel  { }, -- Popup menu: Selected item "extra text"
 		-- PmenuSbar      { }, -- Popup menu: Scrollbar.
 		-- PmenuThumb     { }, -- Popup menu: Thumb of the scrollbar.
-		Question { fg = green2 },                        -- |hit-enter| prompt and yes/no questions
+		Question { fg = green2 },                                    -- |hit-enter| prompt and yes/no questions
 		-- QuickFixLine   { }, -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
-		Search { gui = "bold, underdashed", fg = green2 }, -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
+		Search { gui = "bold, underdashed", bg = base, fg = yellow1 }, -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
 		-- SpecialKey     { }, -- Unprintable characters: text displayed differently from what it really is. But not 'listchars' whitespace. |hl-Whitespace|
 		-- SpellBad       { }, -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.
 		-- SpellCap       { }, -- Word that should start with a capital. |spell| Combined with the highlighting used otherwise.
@@ -291,7 +291,7 @@ local theme = lush(function(injected_functions)
 
 
 		TelescopeMatching { Search },
-		TelescopeSelection { bg = highlight },
+		TelescopeSelection { bg = yellow1, fg = base },
 		TelescopePromptPrefix {},
 		TelescopePromptNormal { fg = info },
 		TelescopeResultsNormal { fg = dormant },
